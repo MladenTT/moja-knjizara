569c64db62296713bf0dd988e2a7d4bae48ef5aa
MladenTT <mladen94trivunovic@gmail.com>
2025-09-12 22:08:01 +0200

    Proba3

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -4,21 +4,11 @@ import "./Add.css";
 function Add() {
   return (
     <div className="reklama1">
-      {/* Horizontalna slika za desktop */}
-      <img
-        className="horizontal"
-        src={process.env.PUBLIC_URL + "/slike/reklame/Kapetan-Gacerone-1400x300px.jpg"}
-        alt="Horizontalna reklama"
-      />
-
-      {/* Vertikalna slika za mobilni */}
-      <img
-        className="vertical"
-        src={process.env.PUBLIC_URL + "/slike/reklame/Kapetan-Gacerone-800x900px.jpg"}
-        alt="Vertikalna reklama"
-      />
+      <img className="horizontal" src="/slike/reklame/Kapetan-Gacerone-1400x300px.jpg" alt="Horizontalna reklama" />
+      <img className="vertical" src="/slike/reklame/Kapetan-Gacerone-800x900px.jpg" alt="Vertikalna reklama" />
     </div>
   );
 }
+
 
 export default Add;
