3b43befb791deddd5cf826f660ee5be8eab69bb6
MladenTT <mladen94trivunovic@gmail.com>
2025-10-10 22:01:48 +0200

    HashRouter update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Pocetna from "./pages/Pocetna"
 import Stranica from "./pages/Stranica"
 import Book from './pages/Book';
 import NoviArtikal from './pages/NoviArtikal';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { HashRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
 
   return (
     
       <>
-        <BrowserRouter>  
+        <HashRouter>  
             <div className="my-texture"></div>
             <Routes>
               <Route path="*" element={<Pocetna />} /> {/* fallback za sve rute */}
@@ -20,7 +20,7 @@ function App() {
               <Route path='/book' element = { <Book/>}></Route>
               <Route path='/novi-artikal' element = { <NoviArtikal/>}></Route>
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
       </>
   )
 }
