30eae80efdc0cb6753652f975deb41ff5ab80773
MladenTT <mladen94trivunovic@gmail.com>
2025-10-10 21:39:44 +0200

    Router update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/moja-knjizara/', // obavezno ime repozitorijuma
   plugins: [react()]
 })
